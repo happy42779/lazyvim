526223a341e91fb7b929ada522c87910accd1aac
Ian Gong <emailforgong@gmail.com>
2023-03-09 10:43:02 +0800

    Added another keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,14 +5,14 @@
 local Util = require("lazyvim.util")
 
 local function map(mode, lhs, rhs, opts)
-  local keys = require("lazy.core.handler").handlers.keys
-  ---@cast keys LazyKeysHandler
-  -- do not create the keymap if a lazy keys handler exists
-  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
-    opts = opts or {}
-    opts.silent = opts.silent ~= false
-    vim.keymap.set(mode, lhs, rhs, opts)
-  end
+    local keys = require("lazy.core.handler").handlers.keys
+    ---@cast keys LazyKeysHandler
+    -- do not create the keymap if a lazy keys handler exists
+    if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+        opts = opts or {}
+        opts.silent = opts.silent ~= false
+        vim.keymap.set(mode, lhs, rhs, opts)
+    end
 end
 
 -- self-defined keymaps
@@ -21,22 +21,22 @@ map("n", "<S-h>", "0", { desc = "Move to the begin of line" })
 map("n", "<S-l>", "$", { desc = "Move to the end of line" })
 
 -- fast move while editing, useful for typing ; after ()
--- map("i", "<A-a>", "<End>", { desc = "Move to the end of line" })
+map("i", "<A-a>", "<End>", { desc = "Move to the end of line" })
 --
 --
 -- buffers
 if Util.has("bufferline.nvim") then
-  -- map("n", "<S-h>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev buffer" })
-  -- map("n", "<S-l>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer" })
-  map("n", "<A-,>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev buffer" })
-  map("n", "<A-.>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer" })
-  map("n", "<A-,>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev buffer" })
-  map("n", "<A-w>", "<cmd>bdelete!<cr>", { desc = "Close buffer" })
+    -- map("n", "<S-h>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev buffer" })
+    -- map("n", "<S-l>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer" })
+    map("n", "<A-,>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev buffer" })
+    map("n", "<A-.>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer" })
+    map("n", "<A-,>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev buffer" })
+    map("n", "<A-w>", "<cmd>bdelete!<cr>", { desc = "Close buffer" })
 else
-  map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
-  map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next buffer" })
-  map("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
-  map("n", "]b", "<cmd>bnext<cr>", { desc = "Next buffer" })
+    map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
+    map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next buffer" })
+    map("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
+    map("n", "]b", "<cmd>bnext<cr>", { desc = "Next buffer" })
 end
 -- map("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
 -- map("n", "<leader>`", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
